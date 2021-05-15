1f3bc759eb6b92c84ca739b9da3b409eb2396706
Oleh Statkevych <olegstatkevychb@gmail.com>
2021-05-15 11:29:33 +0300

    update counter

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,3 +1,18 @@
-// import Vue from './vendor/vue.esm.browser.js';
+
+import Vue from './vendor/vue.esm.browser.js';
+
 
 // Требуется создать Vue приложение
+new Vue({
+  el: '#app',
+  data:() => {
+    return {
+      count: 0
+    }
+  },
+  methods:{
+    updateCount(){
+      this.count++;
+    }
+  }
+})
