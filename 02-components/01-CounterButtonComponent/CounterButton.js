14b56ae595a1171a7f398ddaa4469515916ce22c
Oleh Statkevych <olegstatkevychb@gmail.com>
2021-05-22 18:07:38 +0300

    update counter component

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,14 +1,32 @@
 const CounterButton = {
   name: 'CounterButton',
-
+  
   // Компонент должен иметь входной параметр
+  props:{
+    count:{
+      default:0
+    }
+  },
+
 
   // Компонент должен иметь модель
+  model:{
+    props:'value',
+    event:'increment'
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
+  methods:{
+    incrementValue(e){
+      let counter  = e.target.value;
+      counter++;
+      this.$emit('increment',counter)
+    }
+  },
+
   // Шаблон потребуется отредактировать
-  template: `<button type="button"></button>`,
+  template: `<button :value="count" @click="incrementValue" type="button">+</button>`,
 };
 
 export default CounterButton;
