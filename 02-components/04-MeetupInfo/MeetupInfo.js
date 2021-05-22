b9f51321c57d320cde2b75b513f7493e59006212
Oleh Statkevych <olegstatkevychb@gmail.com>
2021-05-22 18:47:03 +0300

    update meetup info component

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,44 @@
 export default {
   name: 'MeetupInfo',
 
+  props:{
+    organizer:{
+      String,
+      required:true
+    },
+    place:{
+      String,
+      required:true
+    },
+    date:{
+      Date,
+      required:true
+    }
+  },
+
+  computed:{
+    localizedDate(){
+      return new Date(this.date).toLocaleString(navigator.language,{
+        year:'numeric',
+        month:'long',
+        day:'numeric'
+      })
+    }
+  },
+
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{localizedDate}}</time>
       </li>
     </ul>`,
 };
